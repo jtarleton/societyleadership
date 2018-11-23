e04fef6d4b01f9347d6309d76c223a63fd0afaf4
System Administrator <root@jamess-Air.attlocal.net>
2018-11-23 11:04:51 -0500

    add login form, authenticated user to session

---
+++ _login_form.php
@@ -0,0 +1,5 @@
+<form action="/member/login" method="POST">
+        <label for="username_login">Username <input name="username_login" type="text"></input></label>
+        <label for="username_password">Password <input name="username_password" type="text"></input></label>
+        <input type="submit" value="Login"></input>
+    </form>
