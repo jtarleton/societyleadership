ed705c64914b81d12344d15bfe48d8a001fdcd4d
System Administrator <root@jamess-Air.attlocal.net>
2018-11-23 03:49:42 -0500

    add validator

---
+++ Validator.php
@@ -0,0 +1,50 @@
+<?php 
+
+class Validator {
+	
+	/**
+	 * @param string
+	 * @return boolean
+	 */
+	public function validateStringEmail($email) {
+		if (filter_var($email, FILTER_VALIDATE_EMAIL)) { 
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @param string
+	 * @return bool
+	 */
+	public function validateStringNotEmpty($string) {
+		$string = trim($string);
+		if (!empty($string) && is_string($string)) {
+			return true;
+		}
+		return false;
+	}
+
+	public function validateStringLength($string, $limit = 6) {
+		// only pass if the string is at least $limit 
+		// characters in length
+		if (strlen($string) >= (int) $limit) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @param Object
+	 * @return boolean
+	 */
+	public function validateUserNoneExists(User $user) {
+		// check the user does not already exist in the DB
+		// assume the user may exist
+		$noneExists = false;
+
+		// if not found, set to true. 
+		return $noneExists;
+
+	}
+}
