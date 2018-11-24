705d72212f7494c0473f454627a334ec4253eb98
James Tarleton <jtarleton@ad-id.org>
2018-11-24 14:53:56 -0500

    move classes into their own files, reorg directory structure

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ global $ini_array;
 $ini_array = parse_ini_file(__DIR__ . '/society_leadership_config.ini', true);
 
 //load libraries
-require(__DIR__ . '/SocietyDB.php');
-require(__DIR__ . '/User.php');
-require(__DIR__ . '/Route.php');
-require(__DIR__ . '/Validator.php');
-require(__DIR__ . '/society_lib.php');
+require(__DIR__ . '/src/SocietyDB.php');
+require(__DIR__ . '/src/User.php');
+require(__DIR__ . '/src/Route.php');
+require(__DIR__ . '/src/Validator.php');
+require(__DIR__ . '/src/society_lib.php');
 
 // Send output to browser
 \SocietyLeadership\render_view();
