7bb7794afab2b75d05dff6ea450de94a70bd863a
James Tarleton <jtarleton@ad-id.org>
2018-11-24 21:16:28 -0500

    add ActionsControllers.

--- src/_login_form.php
+++
@@ -1,5 +0,0 @@
-<form action="/member/login" method="POST">
-        <label for="username_login">Username <input value="jtarleton" name="username_login" type="text"></input></label>
-        <label for="username_password">Password <input value="jtarleton" name="username_password" type="password"></input></label>
-        <input type="submit" value="Login"></input>
-    </form>
